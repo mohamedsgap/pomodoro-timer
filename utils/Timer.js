51d3e7dedb8b1cf667907e81abfeaf2dcdf45257
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-12-10 18:45:23 +0200

    feat: tick

--- utils/Timer.js
+++ utils/Timer.js
@@ -1,17 +1,32 @@
 export default class Timer {
-    constructor(onTick, onEnd, duration){
-        this.onTick = onTick
-        this.onEnd = onEnd
-        this.duration = duartion
-        this.endTime =  Date.now() + duration
-    }
+  constructor(onTick, onEnd, duration) {
+    this.onTick = onTick;
+    this.onEnd = onEnd;
+    this.duration = duartion;
+    this.endTime = Date.now() + duration;
+  }
 
-    get timeRemaining(){
-        return this.endTime - Date.now()
-    }
+  get timeRemaining() {
+    return this.endTime - Date.now();
+  }
 
-    get isRunning() {
-        return !!this.endTime
-    }
+  get isRunning() {
+    return !!this.endTime;
+  }
 
+  clearTick = () => {
+    clearTimeout(this.timeout);
+    this.timeout = null;
+  };
+
+  tick = () => {
+    if (this.endTime < Date.now()) {
+      this.onTick(0);
+      this.onEnd();
+    } else {
+      this.onTick(this.timeRemaining);
+      const nextTick = this.timeRemaining % 1000;
+      this.timeout = setTimeout(this.tick, nextTick);
+    }
+  };
 }
