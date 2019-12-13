22ef944ef5d423468600f8f4c133e6271030edbb
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-12-13 07:18:57 +0200

    import necessary components

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {Countdown , TimeInput, TimerToggleButton} from './components';
+import {Timer, vibrate} from './utils'
+
+const DEFAULT_WORK_MINS = 25
+const DEFAULT_BREAK_MINS = 5
+
+const minToSec = mins => mins * 60
+nextTimer = {work: 'break', break: 'work'}
 
 export default class App extends React.Component {
   render() {
